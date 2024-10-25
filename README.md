137717ee6176db28c55658e547884bd90811665b
David Roussov <david.roussov@protonmail.com>
2024-10-25 11:40:02 +1100

    add invoke script

---
+++ README.md
@@ -0,0 +1,15 @@
+# Type inference and conversion
+
+## Prerequisites
+
+## Setup
+
+1. Clone the repository: 
+```bash
+git clone https://github.com/davidRoussov/demo_server.git
+```
+
+## Invoke
+
+./scripts/invoke.sh ./datasets/sample_data.csv | jq '.'
+
---
+++ scripts/invoke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 /path/to/your/data.csv"
+  exit 1
+fi
+
+FILE_PATH="$1"
+URL="http://localhost:8000/typeconverter/uploads/"
+
+ curl -X POST "$URL" \
+   -F "file=@${FILE_PATH}" \
+   -H "Content-Type: multipart/form-data"
+
